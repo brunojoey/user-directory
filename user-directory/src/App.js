d1f548f279f0b0820e12637698262f0fabc510b1
Joey Bruno <brunojoey12@gmail.com>
2020-04-15 22:18:17 -0500

    Got full employees json to show on screen.

--- user-directory/src/App.js
+++ user-directory/src/App.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { Component } from "react";
 import Table from "./components/Table";
 import Wrapper from "./components/Wrapper"
+import employees from "./employees.json"
 
-function App() {
-  return (
-    <Wrapper>
-      <h1>Company Employee Directory</h1>
-      <Table />
-    </Wrapper>
-  )
+class App extends Component {
+  state = {
+    employees
+  };
+
+  render() {
+
+    return (
+      <Wrapper>
+        <h1>Company Employee Directory</h1>
+        {this.state.employees.map(employee => (
+          <Table
+            id={employee.id}
+            key={employee.id}
+            name={employee.name}
+            occupation={employee.occupation}
+            salary={employee.salary}
+          />
+        ))
+        };
+      </Wrapper>
+    )
+  }
+
 }
 
 export default App;
