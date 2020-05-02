c0d9d14d72fd4d6ddb7a689a7e705a44924b1735
Joey Bruno <brunojoey12@gmail.com>
2020-05-02 16:32:17 -0500

    Updates

---
+++ precache-manifest.849945cd61fb0c2e857cefda4fb57cb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42f5029f211883560175175bc710f53b",
+    "url": "/user-directory/index.html"
+  },
+  {
+    "revision": "9454042ef5ae2c3be76d",
+    "url": "/user-directory/static/css/main.15b8233c.chunk.css"
+  },
+  {
+    "revision": "644b93fbff12784868b5",
+    "url": "/user-directory/static/js/2.f5619840.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/user-directory/static/js/2.f5619840.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9454042ef5ae2c3be76d",
+    "url": "/user-directory/static/js/main.332c1991.chunk.js"
+  },
+  {
+    "revision": "e29a53fa35a87a84a888",
+    "url": "/user-directory/static/js/runtime-main.8b315796.js"
+  }
+]);
