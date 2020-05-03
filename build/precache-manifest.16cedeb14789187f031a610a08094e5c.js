1a329dac6c0553a8736430288132b932919918f9
Joey Bruno <brunojoey12@gmail.com>
2020-05-02 21:15:12 -0500

    Removed an instance of text-center.

---
+++ build/precache-manifest.16cedeb14789187f031a610a08094e5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6eecf111306238a9c504e78f47988185",
+    "url": "/user-directory-build/index.html"
+  },
+  {
+    "revision": "365876474077b8a33205",
+    "url": "/user-directory-build/static/css/main.c849a62f.chunk.css"
+  },
+  {
+    "revision": "8dccff13bf43e5a57900",
+    "url": "/user-directory-build/static/js/2.782ff040.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/user-directory-build/static/js/2.782ff040.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "365876474077b8a33205",
+    "url": "/user-directory-build/static/js/main.812412cf.chunk.js"
+  },
+  {
+    "revision": "505c4c9a04e4ad108740",
+    "url": "/user-directory-build/static/js/runtime-main.2960b32b.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/user-directory-build/precache-manifest.1df093c604cd7de25d79104a5949c3bb.js"
+  "/user-directory-build/precache-manifest.16cedeb14789187f031a610a08094e5c.js"
 );
 
 self.addEventListener('message', (event) => {
